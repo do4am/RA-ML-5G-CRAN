e571b7b3525498e54dc34a398531e6789466bad4
Nam Do <dominhhoangnam@gmail.com>
2019-04-23 10:37:40 +0200

    Last Change

--- Common/group_samples_in_class.m
+++
@@ -1,9 +0,0 @@
-%function 
-function number_classes_total = group_samples_in_class(class_i, number_classes)   
-    iter_pos = size(class_i,1);
-    number_classes_total = zeros(iter_pos,1);
-    pos_idx = class_i(:,1);
-
-    for j = 1:iter_pos
-        number_classes_total(j) = number_classes(pos_idx(j)); 
-    end
